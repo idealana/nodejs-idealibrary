7c46d1bf05e8b3d7301c786b5f8746b9c608dfb6
idealana <idealana@example>
2025-05-22 21:55:54 +0700

    add book route

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+/** Routes */
+const bookRoute = require('./routes/book');
+
 const app = express();
 
 app.use(express.json());
+
+/** Book Route */
+app.use('/api/book', bookRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
