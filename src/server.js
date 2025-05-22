790276c7689015214ac26ef34cf3554e0ea956a9
idealana <idealana@example>
2025-05-22 21:56:13 +0700

    add 404 handler

--- src/server.js
+++ src/server.js
@@ -10,6 +10,12 @@ app.use(express.json());
 /** Book Route */
 app.use('/api/book', bookRoute);
 
+app.use((req, res, next) => {
+	res.status(404).json({
+		message: 'Resource not found',
+	});
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 	console.log(`Application running on port ${PORT}`);
