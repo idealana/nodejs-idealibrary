ec9e4d917f8b59a4190ef06f84a86c6947e106a6
idealana <lanarf97@gmail.com>
2025-06-02 20:50:22 +0700

    add BaseRepository class

---
+++ src/repositories/Base.js
@@ -0,0 +1,133 @@
+const prisma = require('../configs/database');
+
+class BaseRepository {
+
+	#isWithTrashed = false;
+	
+	/**
+	 * @param string modelName
+	 */
+	constructor(modelName) {
+		this._modelName = modelName;
+		this._isSoftDeletes = false;
+	}
+
+	/**
+	 * Set With Trashed
+	 * @param boolean value
+	 * @return class
+	 */
+	withTrashed(value = true) {
+		this.#isWithTrashed = value;
+		return this;
+	}
+
+	/**
+	 * Is Soft Deletes and With Trashed
+	 * @return boolean
+	 */
+	isWithTrashed() {
+		return (this._isSoftDeletes && this.#isWithTrashed);
+	}
+
+	/**
+	 * Get All Data
+	 * @param object where
+	 * @return array objects
+	 */
+	async getAll(where = {}) {
+		if(!this.isWithTrashed()) {
+			where.deletedAt = null;
+		}
+
+		this.withTrashed(false); // reset
+
+		return await prisma[this._modelName].findMany({
+			where,
+		});
+	}
+
+	/**
+	 * Find Data
+	 * @param object where
+	 * @return object
+	 */
+	async findBy(where) {
+		if(!this.isWithTrashed()) {
+			where.deletedAt = null;
+		}
+
+		this.withTrashed(false); // reset
+
+		return await prisma[this._modelName].findUnique({
+			where,
+		});
+	}
+
+	/**
+	 * Find Data by id
+	 * @param int id
+	 * @return object
+	 */
+	async findById(id) {
+		return await this.findBy({ id });
+	}
+
+	/**
+	 * Insert Data
+	 * @param object data
+	 * @return object
+	 */
+	async insert(data){
+		return await prisma[this._modelName].create({ data });
+	}
+
+	/**
+	 * Update Data
+	 * @param object where
+	 * @param object data
+	 * @return object
+	 */
+	async updateBy(where, data){
+		return await prisma[this._modelName].update({
+			where,
+			data,
+		});
+	}
+
+	/**
+	 * Update Data by id
+	 * @param int id
+	 * @param object data
+	 * @return object
+	 */
+	async updateById(id, data){
+		return await this.updateBy({ id }, data);
+	}
+
+	/**
+	 * Hard or Soft Delete Data
+	 * @param object where
+	 * @param boolean isForceDelete
+	 * @return mix object|boolean
+	 */
+	async delete(where, isForceDelete = false) {
+		if(this._isSoftDeletes && !isForceDelete) {
+			return await this.updateBy(where, { deletedAt: new Date() });
+		}
+
+		return await prisma[this._modelName].delete({ where });
+	}
+
+	/**
+	 * Delete Data by id
+	 * @param int id
+	 * @param boolean isForceDelete
+	 * @return mix object|boolean
+	 */
+	async deleteById(id, isForceDelete = false) {
+		return await this.delete({ id }, isForceDelete);
+	}
+}
+
+module.exports = BaseRepository;
