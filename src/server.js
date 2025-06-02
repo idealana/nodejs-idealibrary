e645b3299e57af9272136d9ce0294b2227c08e25
idealana <lanarf97@gmail.com>
2025-06-02 20:51:45 +0700

    add category route

--- src/server.js
+++ src/server.js
@@ -17,6 +17,9 @@ app.use('/auth', authRoute);
 /** Book Route */
 app.use('/api/book', AuthMiddleware.authenticateToken, bookRoute);
 
+/** Category Route */
+app.use('/api/category', AuthMiddleware.authenticateToken, require('./routes/category'));
+
 app.use((req, res, next) => {
 	res.status(404).json({
 		message: 'Resource not found',
